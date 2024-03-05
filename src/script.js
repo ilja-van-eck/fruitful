d2aa040d8e8cf32c83058504a4ec40eebc48a4b4
Ilja van Eck <122357631+ilja-van-eck@users.noreply.github.com>
2024-03-05 23:25:59 +0000

    Testing stuff

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log("test 2");
+// GLOBAL
+const isMobile = window.innerWidth < 480;
+const isMobileLandscape = window.innerWidth < 768;
+const timestamps = [0, 3, 6, 9, 12, 15, 18, 20, 22, 24];
+const loadWrap = document.querySelector(".load-w");
+const pageOverlay = document.querySelector(".page-overlay");
+const loadBg = loadWrap.querySelector(".load-bg");
+const navW = document.querySelector(".nav-w");
+let titleLines;
+let closeMenu;
+let ranHomeLoader = false;
+let generalFlag = false;
+let mobileMenuOpen = false;
