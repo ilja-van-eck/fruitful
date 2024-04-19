39afd03d31b7bb5239b92d3bfbe2c08190061705
Ilja van Eck <122357631+ilja-van-eck@users.noreply.github.com>
2024-04-19 18:15:53 +0000

    style: Remove unnecessary timestamps array

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@
 const isMobile = window.innerWidth < 480;
 const isMobileLandscape = window.innerWidth < 768;
 const isDesktop = window.innerWidth > 991;
-const timestamps = [0, 3, 6, 9, 12, 15, 18, 20, 22, 24];
 const loadWrap = document.querySelector(".load-w");
 const pageOverlay = document.querySelector(".page-overlay");
 const loadBg = loadWrap.querySelector(".load-bg");
